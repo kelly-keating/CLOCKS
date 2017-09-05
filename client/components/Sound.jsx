11c1cb70f64563f93dd6a0bf4f527454122a6fe2
kelly-keating <keating.kel@gmail.com>
2017-09-05 23:17:12 +1200

    sound added but vol missing

--- client/components/Sound.jsx
+++ client/components/Sound.jsx
@@ -7,9 +7,17 @@ class Sound extends Clock {
     super(props)
   }
 
+  updateVol(s) {
+    console.log((s+1)/60);
+  }
+
   render() {
     return(
-      <div className='clock'>Sound</div>
+      <div className='clock'>
+        Sound
+        {this.updateVol(this.state.s)}
+        <audio id='secondSound' src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Beep_example.ogg" type="audio/wav" autoPlay='true' loop='true' volume='0.5'/>
+      </div>
     )
   }
 }
